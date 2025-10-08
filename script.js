f3cbd2ef8b8cdceb2c0210a19ad0c3da6e011907
moenabischoff <moena.bischoff@gmail.com>
2025-10-08 15:18:36 +0200

    styles

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("Hello, World!");
 
-fetch("https://bebbiparking.ramisberger-tabea.ch/unload.php") 
+fetch("https://bebbiparking.ramisberger-tabea.ch/unload.php")
     .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
 
